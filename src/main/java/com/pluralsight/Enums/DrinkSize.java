cf4474a7e453fca54950917677d43761fe8ac992
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-28 11:52:40 -0500

    added getter for culture theme and different cultures

--- src/main/java/com/pluralsight/Enums/DrinkSize.java
+++ src/main/java/com/pluralsight/Enums/DrinkSize.java
@@ -1,4 +1,20 @@
 package com.pluralsight.Enums;
 
 public enum DrinkSize {
+
+    // drink sizes
+    SMALL(1.00),
+    MEDIUM(3.00),
+    LARGE(5.00);
+
+    private final double price;
+
+    // constructor for price
+    DrinkSize(double price) {
+        this.price = price;
+    }
+    // getter for price
+    public double getPrice() {
+        return price;
+    }
 }
