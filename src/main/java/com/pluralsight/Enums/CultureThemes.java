916d3c35f6412ba124dc89fccc99934334cb5131
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-28 11:48:53 -0500

    added getter for culture theme and different cultures

--- src/main/java/com/pluralsight/Enums/CultureThemes.java
+++ src/main/java/com/pluralsight/Enums/CultureThemes.java
@@ -1,4 +1,29 @@
 package com.pluralsight.Enums;
 
 public enum CultureThemes {
+    //add different culture themes customers can choose from
+    SOUTHERN_CAROLINA_LOWCOUNTRY("Southern Comfort Food"),
+    YORUBA_NIGERIAN("Yoruba Nigerian Cuisine"),
+    SALKEHATCHIE("Salkehatchie Cuisine"),
+    SOUTH_INDIAN_GUJARATI("South Indian Gujarati Cuisine"),
+    LEVANTINE_MEDITERRANEAN("Levantine Mediterranean Cuisine"),
+    ITAL_CARRIBBEAN("Ital Caribbean Cuisine"),
+    OKINAWAN_JAPANESE("Okinawan Japanese Cuisine"),
+    BRAZILLIAN("Brazilian Cuisine"),
+    DUTCH;
+
+    CUISINE("Dutch Cuisine");
+
+    private final String displayName;
+
+
+    //add constructor for display name
+    CultureThemes(String displayName) {
+        this.displayName = displayName;
+    }
+
+    //add getter for display name
+    public String getDisplayName() {
+        return displayName;
+    }
 }
