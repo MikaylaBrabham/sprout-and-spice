b3bddf2d5180a46a5eda5861d45b3b07390c45ac
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-28 12:37:31 -0500

    added diff menuitems

--- src/main/java/com/pluralsight/Models/MenuItem.java
+++ src/main/java/com/pluralsight/Models/MenuItem.java
@@ -1,4 +1,22 @@
 package com.pluralsight.Models;
 
-public class MenuItem {
+public abstract class MenuItem {
+
+    //get name
+    private String name;
+
+    // add constructor for name
+    public MenuItem(String name) {
+        this.name = name;
+    }
+
+    //add getter for name
+    public String getName() {
+        return name;
+    }
+
+    // add abstract method for price and details
+    public abstract double getPrice();
+    public abstract String getMenuDetails();
+
 }
