701ff7b1ea300b5f59d1498be76ff3ab76d31a85
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-29 00:29:24 -0500

    added total price and order details with the given formatting

--- src/main/java/com/pluralsight/Models/Order.java
+++ src/main/java/com/pluralsight/Models/Order.java
@@ -14,9 +14,10 @@ public class Order {
     }
 
     //add menu item to order
-public void addMenuItem(MenuItem menuItem) {
-    this.menuItems.add(menuItem);
-}
+    public void addMenuItem(MenuItem menuItem) {
+        this.menuItems.add(menuItem);
+    }
+
     //add is empty method
     public boolean isEmpty() {
         return menuItems.isEmpty();
@@ -26,6 +27,7 @@ public void addMenuItem(MenuItem menuItem) {
     public boolean hasBowl() {
         return menuItems.stream().anyMatch(menuItem -> menuItem instanceof MosaicBowl);
     }
+
     //add method to check if order has drink or side; if no bowl they must get both
     public boolean hasDrinkOrSideDish() {
         return menuItems.stream().anyMatch
@@ -35,13 +37,31 @@ public void addMenuItem(MenuItem menuItem) {
     //add totalprice to get total
     public double getTotalPrice() {
         return menuItems.stream().mapToDouble(MenuItem::getPrice).sum();
-}
+    }
 
-//add order details to get newest to oldest orders in the given format
+    //add order details to get newest to oldest orders in the given format
     public String getOrderDetailsAndFormat() {
         //add array for menuitems
         ArrayList<MenuItem> reversedMenuItems = new ArrayList<>(menuItems);
         Collections.reverse(reversedMenuItems);
+
+        //format order details and create output
+        String orderDetails = reversedMenuItems.stream()
+                .map(MenuItem::getMenuDetails)
+                .collect(Collectors.joining("\n"));
+
+        //basic ouptut for order details
+        return """
+                ======================================
+                            SPROUT & SPICE
+                            **Mosaic Meals**
+                ======================================
+                """ + orderDetails +
+                "\n\nTotal: $" + String.format("%.2f", getTotalPrice()) +
+                "\n\nThank you for dining with us!";
+        "\n======================================\n";
+    }
+}
 
 
     }
