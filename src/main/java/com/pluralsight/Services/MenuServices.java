c75acd84e11da9068c6a712fbf36b630b56e224f
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-29 00:32:42 -0500

    added plan

--- src/main/java/com/pluralsight/Services/MenuServices.java
+++ src/main/java/com/pluralsight/Services/MenuServices.java
@@ -1,4 +1,14 @@
 package com.pluralsight.Services;
 
+
+//import my packages
+
+//import hashmap, lists, and maps
 public class MenuServices {
+
+    //add properties
+
+    //add constructors
+
+
 }
