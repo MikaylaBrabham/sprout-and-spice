9de85bf5ee0b8df6e30dc5ac2f498df0a695ce64
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-29 03:06:25 -0500

    added constructors

--- src/main/java/com/pluralsight/Services/SignatureBowlService.java
+++ src/main/java/com/pluralsight/Services/SignatureBowlService.java
@@ -13,4 +13,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SignatureBowlService {
+
+    //add strings
+    private Map<CultureThemes, MosaicBowl> signatureBowls;
+    private MenuServices menuServices;
+
+    //add constructor
+
+    public SignatureBowlService(MenuServices menuServices) {
+        signatureBowls = signatureBowls;
+        this.menuServices = menuServices;
+        loadSignatureBowls();
+    }
 }
